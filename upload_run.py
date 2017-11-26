a9b5bb2bfd3d496e2e24a096cde93148980f7e66
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-11-26 13:48:53 -0300

    1
    1

--- upload_run.py
+++ upload_run.py
@@ -1,17 +1,25 @@
 import dropbox
+import os
 
-client = dropbox.client.DropboxClient('1bF6SpHPebAgAAAAAAABDuS1GCQ8QBXEgvxBDj4X1EdwLLVlTiE54wdcoLP1dtjaQ')
-print 'linked account: ', client.account_info()
+dbx = dropbox.Dropbox('bF6SpHPebAgAAAAAAABGfQBin3OqrxpM0EdiXFE-C5GRJwrLy9Nh12135DHwXmKFtg1')
+dbx.users_get_current_account()
 
-f = open('2.zip', 'rb')
-response = client.put_file('/magnum-opus.zip', f)
-print 'uploaded: ', response
+# Fichero
+file = "26_11_2017_14_03_00_CNP.zip"
 
-folder_metadata = client.metadata('/')
-print 'metadata: ', folder_metadata
+tamano_maximo_megas = 50
+CHUNK_SIZE = tamano_maximo_megas * 1024 * 1024
+file_size = os.path.getsize(file)
 
-f, metadata = client.get_file_and_metadata('/magnum-opus.txt')
-out = open('magnum-opus.zip', 'wb')
-out.write(f.read())
-out.close()
-print metadata
+print CHUNK_SIZE
+print file_size
+
+if file_size <= CHUNK_SIZE:
+    # Upload
+    f = open(file, 'rb')
+    response = dbx.files_upload( f.read(), '/' + file)
+    print 'uploaded: ', response
+
+    # Imprimimos la fecha de subida
+    dropbox_fecha_subida = dbx.files_get_metadata('/'+file).server_modified
+    print dropbox_fecha_subida
